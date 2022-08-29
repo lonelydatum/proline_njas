9b90ac2b6cb6e5b93ea99da834a5d153bcf607e7
Gar <gar@lonelydatum.com>
2022-08-29 13:07:38 -0400

    on

--- _assets/_gulp/gulp-deploy.js
+++ _assets/_gulp/gulp-deploy.js
@@ -72,9 +72,9 @@ function log_free(projectName){
         .pipe(replace('data:image/svg+xml;utf8', 'data:image/svg+xml;charset=utf-8'))
         .pipe(replace('<script type="text/javascript" src="http://localhost:48626/takana.js"></script>', ''))
         .pipe(replace("takanaClient.run({host: 'localhost:48626'});", ''))
-        .pipe(replace("../_common/images/"+size+"/"+lang+"/"+name+"/", ''))
-        .pipe(replace("../_common/images/"+size+"/"+lang+"/", ''))        
-        .pipe(replace("../_common/images/"+size+"/"+eastWest+"/", ''))
+        
+        
+        .pipe(replace("../_common/images/"+size+"/"+name+"/", ''))
         .pipe(replace("../_common/images/"+size+"/", ''))
         .pipe(replace("txt/", ''))
         .pipe(replace("<title>", '<title>Created: '+moment().format('MMM D, h:mm')))
