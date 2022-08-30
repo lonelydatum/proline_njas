71dda8dafdd7a7f5b99c1c124e027d9f0b47c807
Gar <gar@lonelydatum.com>
2022-08-29 22:03:37 -0400

    on

--- dev/_common/js/common.js
+++ dev/_common/js/common.js
@@ -64,20 +64,55 @@ function b_300x250(){
 	
 }
 
+function origin(el, x, y) {
+	TweenLite.set(el, {transformOrigin:`${x}px ${y}px`, x:`${-x/2}px`, y:`${-y/2}px`, scale:.5})
+
+}
+
+function tint(){
+	const tl = new TimelineMax()
+	const y = 15
+	const duration = .3
+	tl.add("t1-scale-start")
+	tl.to(".t1a", {duration, ease:"power3.easein", y:`-=${y}`}, "t1-scale-start")
+	tl.to(".t1b", {duration, ease:"power3.easein", scale:.7}, "t1-scale-start")
+	tl.to(".t1b feFlood", {duration:.3, floodColor:'#ffd100'}, "t1-scale-start")
+
+	tl.add("t1-scale-normal", "+=.3")
+	tl.to(".t1a", {duration:.2, ease:"power3.easein", y:`+=${y}`}, "t1-scale-normal")
+	tl.to(".t1b", {duration:.2, ease:"power3.easein", scale:.5}, "t1-scale-normal")
+	tl.to(".t1b feFlood", {duration:.3, floodColor:'#ffffff'}, "t1-scale-normal")
+	return tl
+}
+
 
 
 function b_300x250__(){	
+	origin(".t1b", 300, 320)
+
+	
+
 	const tl = init()
-	tl.add("f1")
+	// tl.add("f1")
 	
 	// tl.from(".hero", {duration:.4, x:"+=200", opacity:0, ease:"power4.out"}, "f1")
-	// tl.from(".logo1", {duration:.4, x:"+=200", opacity:0}, "f1+=.15")
+	// tl.from(".logo1", {duration:.4, x:"+=200", opacity:0}, "f1+=1.15")
+
 	tl.to(".logo1", {duration:.2, opacity:0}, "+=1")
 	tl.add("t1")
 	tl.from(".t1a", {duration:.3, x:"-=130", y:"+=30", opacity:0}, "t1")
-	tl.from(".t1b", {duration:.3, x:"+=130", y:"-=30", opacity:0}, "t1")
-	tl.to([ ".hero", ".bg", ".t1"], {duration:.2, opacity:0}, "+=3")
+	tl.from(".t1b", {duration:.3, x:"+=130", y:"-=30", opacity:0}, "t1+=1")
 
+	tl.add(tint())
+	
+
+	
+	tl.to([ ".hero", ".bg", ".t1"], {duration:.2, opacity:0}, "+=1")
+
+
+	
+	
+	
 
 	tl.from(".logo2", {duration:.3, opacity:0}, "+=.1")
 	tl.from(".t2", {duration:.3, opacity:0}, "+=.3")
