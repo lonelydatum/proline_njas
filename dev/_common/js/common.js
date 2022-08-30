5b63ee912f4c9914ec916780adb707c506c17038
Gar <gar@lonelydatum.com>
2022-08-30 12:31:15 -0400

    on

--- dev/_common/js/common.js
+++ dev/_common/js/common.js
@@ -27,43 +27,12 @@ function init(){
 }
 
 
-function hero(){
-	const tl = init()
-	tl.add("f1")
-	tl.from(".hero", {duration:.4, x:"+=200", opacity:0, ease:"power4.out"}, "f1")
-	tl.from(".logo1", {duration:.4, x:"+=200", opacity:0}, "f1+=.15")
-	tl.to(".logo1", {duration:.2, x:"-=200", opacity:0}, "+=.3")
-
-	tl.from(".t1a", {duration:.3, opacity:0}, "+=.3")
-	tl.from(".t1b", {duration:.3, opacity:0}, "+=.3")
-	tl.to([ ".hero", ".bg", ".t1"], {duration:.2, opacity:0}, "+=2.6")
-	return tl
-}
-
 
 
 function b_160x600(){	
 	b_300x250()
 }
 
-
-
-function b_300x250(){	
-	const tl = hero()
-
-
-	tl.from(".logo2", {duration:.3, opacity:0}, "+=.1")
-	tl.from(".t2", {duration:.3, opacity:0}, "+=.3")
-	tl.to(".njas", {duration:.2, opacity:0}, "+=2.1")
-
-	tl.from([".t3", ".logo3"], {duration:.3, opacity:0}, "+=.2")
-	tl.from([".footer", ".cta"], {duration:.3, opacity:0}, "+=.5")
-
-	tl.add(olg())
-
-	return tl
-	
-}
 
 
 
@@ -78,30 +47,21 @@ function tint(){
 
 	tl.add("t1-scale-normal", "+=.2")
 	tl.to(".t1a", {duration:.2, ease:"power3.easein", y:`+=${y}`}, "t1-scale-normal")
-	tl.to(".t1b", {duration:.2, ease:"power3.easein", scale:.5}, "t1-scale-normal")
-	// tl.to(".t1b feFlood", {duration:.5, floodColor:'#ffffff'}, "t1-scale-normal")
+	tl.to(".t1b", {duration:.2, ease:"power3.easein", scale:.5}, "t1-scale-normal")	
 	return tl
 }
 
 
 
-function b_300x250__(){	
+function b_300x250(){	
 	origin(".t1b", 300, 320)
-
-	
-
 	const tl = init()
-	
-
 	tl.to(".logo1", {duration:.2, opacity:0}, "+=.6")
 	tl.add("t1")
 	tl.from(".t1a", {duration:.3, x:"-=130", y:"+=30", opacity:0}, "t1")
 	tl.from(".t1b", {duration:.3, x:"+=130", y:"-=30", opacity:0}, "t1+=.6")
 
 	tl.add(tint(), "+=.5")
-	
-
-	
 	tl.to([ ".hero", ".bg", ".t1"], {duration:.2, opacity:0}, "+=1")
 
 
@@ -142,4 +102,4 @@ function b_300x600(){
 	
 }
 
-export { init, b_160x600, b_300x250, b_300x600, b_300x250__ }
+export { init, b_160x600, b_300x250, b_300x600 }
--- dev/football_300x600/_bundled/main.js
+++ dev/football_300x600/_bundled/main.js
@@ -29,36 +29,8 @@ function init() {
 	return tl;
 }
 
-function hero() {
-	var tl = init();
-	tl.add("f1");
-	tl.from(".hero", { duration: .4, x: "+=200", opacity: 0, ease: "power4.out" }, "f1");
-	tl.from(".logo1", { duration: .4, x: "+=200", opacity: 0 }, "f1+=.15");
-	tl.to(".logo1", { duration: .2, x: "-=200", opacity: 0 }, "+=.3");
-
-	tl.from(".t1a", { duration: .3, opacity: 0 }, "+=.3");
-	tl.from(".t1b", { duration: .3, opacity: 0 }, "+=.3");
-	tl.to([".hero", ".bg", ".t1"], { duration: .2, opacity: 0 }, "+=2.6");
-	return tl;
-}
-
 function b_160x600() {
 	b_300x250();
-}
-
-function b_300x250() {
-	var tl = hero();
-
-	tl.from(".logo2", { duration: .3, opacity: 0 }, "+=.1");
-	tl.from(".t2", { duration: .3, opacity: 0 }, "+=.3");
-	tl.to(".njas", { duration: .2, opacity: 0 }, "+=2.1");
-
-	tl.from([".t3", ".logo3"], { duration: .3, opacity: 0 }, "+=.2");
-	tl.from([".footer", ".cta"], { duration: .3, opacity: 0 }, "+=.5");
-
-	tl.add((0, _proline.olg)());
-
-	return tl;
 }
 
 function tint() {
@@ -73,22 +45,18 @@ function tint() {
 	tl.add("t1-scale-normal", "+=.2");
 	tl.to(".t1a", { duration: .2, ease: "power3.easein", y: "+=" + y }, "t1-scale-normal");
 	tl.to(".t1b", { duration: .2, ease: "power3.easein", scale: .5 }, "t1-scale-normal");
-	// tl.to(".t1b feFlood", {duration:.5, floodColor:'#ffffff'}, "t1-scale-normal")
 	return tl;
 }
 
-function b_300x250__() {
+function b_300x250() {
 	(0, _helpersHelpersJs.origin)(".t1b", 300, 320);
-
 	var tl = init();
-
 	tl.to(".logo1", { duration: .2, opacity: 0 }, "+=.6");
 	tl.add("t1");
 	tl.from(".t1a", { duration: .3, x: "-=130", y: "+=30", opacity: 0 }, "t1");
 	tl.from(".t1b", { duration: .3, x: "+=130", y: "-=30", opacity: 0 }, "t1+=.6");
 
 	tl.add(tint(), "+=.5");
-
 	tl.to([".hero", ".bg", ".t1"], { duration: .2, opacity: 0 }, "+=1");
 
 	tl.from(".logo2", { duration: .3, opacity: 0 }, "+=.1");
@@ -125,7 +93,6 @@ exports.init = init;
 exports.b_160x600 = b_160x600;
 exports.b_300x250 = b_300x250;
 exports.b_300x600 = b_300x600;
-exports.b_300x250__ = b_300x250__;
 
 },{"./helpers/helpers.js":2,"./proline":3}],2:[function(require,module,exports){
 "use strict";
